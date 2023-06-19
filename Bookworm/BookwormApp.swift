8b9fabb529a622df3ff3f9a9e7594d205e0919f7
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-19 20:10:19 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rishav Gupta on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
