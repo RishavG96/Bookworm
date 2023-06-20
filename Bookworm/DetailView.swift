fda62273c59ab04ca7938fe7a47253d18057db43
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-20 07:34:42 +0530

    Adding Detail View

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,44 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Rishav Gupta on 20/06/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author ?? "Unknown Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
